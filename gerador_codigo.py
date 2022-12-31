50c81e908437cdcb6adc0c996c716074d8af4427
Marcio Martins <marciopmm@gmail.com>
2022-12-30 23:08:58 -0300

    Início do Segundo Turno

--- gerador_codigo.py
+++ gerador_codigo.py
@@ -1,8 +1,8 @@
 import regex as re
 import os
 
-uf = 'Região Norte'
-uf_sigla = 'n'
+uf = 'Ceará'
+uf_sigla = 'ce'
 
 df1 = 'df1' + uf_sigla
 df2 = 'df2' + uf_sigla
@@ -25,37 +25,38 @@ str1 = f"# Carga dos dados dos BUs - {uf}\n" \
         f"{df1} = {df1}[{df1}['CD_ELEICAO'] == 544]\n" \
         f"{df2} = {df2}[{df2}['CD_ELEICAO'] == 545]\n"
 
-str2 = "# Graficos - Total no 1º turno\n" \
-       f"gerar_grafico_porcentagem({df1}, 'Total de Votos - 1º Turno', '{uf}')\n"
+str2 = "# Graficos - Total no 2º turno\n" \
+       f"gerar_grafico_porcentagem({df2}, 'Total de Votos - 2º Turno', '{uf}')\n"
 
 str3 = "# Total de votos acumulados\n" \
-       f"gerar_grafico_temporal({df1}, 'Eleições 2022 - 1º Turno - Contagem temporal para Presidente', '{uf_sigla.upper()}')\n"
+       f"gerar_grafico_temporal({df2}, 'Eleições 2022 - 2º Turno - Contagem temporal para Presidente', '{uf_sigla.upper()}')\n"
 
-str4 = f"{df1}_urnas = {df1}[{df1}['QT_VOTOS'] == 1]\n" \
-       f"print(f\"Urnas com 1 voto no candidato 13: {{{df1}_urnas[{df1}_urnas['NR_VOTAVEL'] == 13]['NR_VOTAVEL'].count()}}\")\n" \
-       f"print(f\"Urnas com 1 voto no candidato 22: {{{df1}_urnas[{df1}_urnas['NR_VOTAVEL'] == 22]['NR_VOTAVEL'].count()}}\")\n"
+str4 = f"{df2}_urnas = {df2}[{df2}['QT_VOTOS'] == 1]\n" \
+       f"print(f\"Urnas com 1 voto no candidato 13: {{{df2}_urnas[{df2}_urnas['NR_VOTAVEL'] == 13]['NR_VOTAVEL'].count()}}\")\n" \
+       f"print(f\"Urnas com 1 voto no candidato 22: {{{df2}_urnas[{df2}_urnas['NR_VOTAVEL'] == 22]['NR_VOTAVEL'].count()}}\")\n"
 
-str5 = f"{df1}_urnas_22 = {df1}_urnas[{df1}_urnas['NR_VOTAVEL'] == 22]\n" \
-       f"{df1}_urnas_22[['NM_MUNICIPIO', 'NR_ZONA', 'NR_SECAO', 'NR_LOCAL_VOTACAO']]\n"
+str5 = f"{df2}_urnas_22 = {df2}_urnas[{df2}_urnas['NR_VOTAVEL'] == 22]\n" \
+       f"print({df2}_urnas_22[['NM_MUNICIPIO', 'NR_ZONA', 'NR_SECAO', 'NR_LOCAL_VOTACAO']])\n"
 
-str6 = f"df_soma = df_diff_group({df1}, {df1}_urnas_22)\n" \
-       f"gerar_grafico_urnas_suspeitas(df_soma, {df1}_urnas_22, '{uf}')\n"
+str6 = f"df_soma = df_diff_group({df2}, {df2}_urnas_22)\n" \
+        "if df_soma is not None:\n" \
+       f"     gerar_grafico_urnas_suspeitas(df_soma, {df2}_urnas_22, '{uf}')\n"
 
-str7 = "# Total de votos acumulados, caso as urnas em que o candidato 22 recebeu 1 voto não fossem computadas\n" \
-       f"{df1}_sub = {df1}[~(({df1}['CD_MUNICIPIO'].isin({df1}_urnas_22['CD_MUNICIPIO'])) & \n" \
-       f"            ({df1}['NR_ZONA'].isin({df1}_urnas_22['NR_ZONA'])) & \n" \
-       f"            ({df1}['NR_SECAO'].isin({df1}_urnas_22['NR_SECAO'])) & \n" \
-       f"            ({df1}['NR_LOCAL_VOTACAO'].isin({df1}_urnas_22['NR_LOCAL_VOTACAO'])))]\n" \
-       f"gerar_grafico_porcentagem({df1}_sub, 'Total de Votos (Caso não se computassem as urnas irregulares) - 1º Turno', '{uf}')\n"
+str7 = "      # Total de votos acumulados, caso as urnas em que o candidato 22 recebeu 1 voto não fossem computadas\n" \
+       f"     {df2}_sub = {df2}[~(({df2}['CD_MUNICIPIO'].isin({df2}_urnas_22['CD_MUNICIPIO'])) & \n" \
+       f"                 ({df2}['NR_ZONA'].isin({df2}_urnas_22['NR_ZONA'])) & \n" \
+       f"                 ({df2}['NR_SECAO'].isin({df2}_urnas_22['NR_SECAO'])) & \n" \
+       f"                 ({df2}['NR_LOCAL_VOTACAO'].isin({df2}_urnas_22['NR_LOCAL_VOTACAO'])))]\n" \
+       f"     gerar_grafico_porcentagem({df2}_sub, 'Total de Votos (Caso não se computassem as urnas irregulares) - 2º Turno', '{uf}')\n"
 
-str8 = "# Total de votos acumulados, caso as urnas em que o candidato 22 recebeu 1 voto não fossem computadas\n" \
-       f"gerar_grafico_temporal({df1}_sub, 'Eleições 2022 - Primeiro Turno - Apuração sem urnas viciadas', '{uf_sigla.upper()}')"
+str8 = "      # Total de votos acumulados, caso as urnas em que o candidato 22 recebeu 1 voto não fossem computadas\n" \
+       f"     gerar_grafico_temporal({df2}_sub, 'Eleições 2022 - Segundo Turno - Apuração sem urnas viciadas', '{uf_sigla.upper()}')"
 
 file = open("generated.txt", "w+")
 
-file.writelines('### ' + uf.upper() + '\n')
-file.writelines(str1)
-file.writelines('\n##### 1.2.1.1. ' + uf.upper() + '\n')
+#file.writelines('### ' + uf.upper() + '\n')
+#file.writelines(str1)
+file.writelines('\n##### 2.x.x.x - ' + uf.upper() + '\n')
 file.writelines(str2)
 file.writelines(str3)
 file.writelines(str4)
